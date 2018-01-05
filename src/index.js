aa79449239f197d839d29477640a7c2f0102e425
seeliang <me@sihuiliang.com>
2018-01-06 10:25:42 +1100

    chore/component: separate loading code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-
-class Root extends React.Component {
-  constructor(props) {
-    super(props);
-    this.click = this.click.bind(this);
-  }
-
-  click() {
-    alert('click this');
-  }
-
-  render() {
-    return (
-      <div>
-        <h1> Hello world</h1>
-        <input type="checkbox"/>
-        <button onClick={this.click}> try this</button>
-      </div>
-    );
-  }
-}
+import Root from './root.js';
 
 render(<Root/>, document.getElementById('app'));
