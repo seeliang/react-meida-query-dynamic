6623246f330ba2e653e8bbd3ca4a5fa4447e2b85
seeliang <me@sihuiliang.com>
2018-09-18 08:32:23 +1000

    feat: init HOC

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Root from './root.js';
+import React, { Component } from 'react';
+import debounce from 'debounce';
 
-render(<Root/>, document.getElementById('app'));
+const getDisplayName = WrappedComponent => WrappedComponent.displayName || WrappedComponent.name || 'Component';
+
+const mQDynamic = (RenderComponent) => {
+  class MQD extends Component {
+    constructor() {
+      super();
+      this.state = {
+        currentWindowWidth: 0,
+      };
+    }
+
+    componentDidMount() {
+      const initWinWidth = window.innerWidth;
+      this.setState({ currentWindowWidth: initWinWidth });
+      window.addEventListener('resize', (e) => {
+        debounce(this.handleResize(e), 300);
+      });
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('resize', this.handleScroll);
+    }
+
+    handleResize(e) {
+      const curWinWidth = e.target.innerWidth;
+      this.setState({ currentWindowWidth: curWinWidth });
+    }
+
+    render() {
+      return <RenderComponent mediaQuery={{ ...this.state }} {...this.props} />;
+    }
+  }
+  MQD.displayName = `MQD(${getDisplayName(RenderComponent)})`;
+
+
+  return MQD;
+};
+
+export default mQDynamic;
